c8f8b2af250c6a2a690f638bc3679dca0bd61096
Alex Bataille <alex.bataille@ulb.be>
2025-09-28 12:30:30 +0200

    split the main function in * init_visualizer * clear_program * main

--- src/main.c
+++ src/main.c
@@ -2,31 +2,25 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <stdbool.h>
+#include "constants.h"
 
 #define SDL_FLAGS SDL_INIT_VIDEO
 
-int main(int argc, char* argv[]) {
-
+struct Visualizer {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+};
+
+bool init_visualizer(struct Visualizer* visualizer_p);
+void clear_program(struct Visualizer* visualizer_p);
+
+int main(int argc, char* argv[]) {
+
+	struct Visualizer main_visualizer;
 	bool running = true;
-	
-	if (!SDL_Init(SDL_FLAGS)) {
-		fprintf(stderr, "Error initializing SDL library: %s\n", SDL_GetError());
-		return 1;
-	}
-	printf("SDL library initalized !\n");
 
-	window = SDL_CreateWindow("AlgoVision", 800, 600, SDL_WINDOW_OPENGL);
-	if (window == NULL) {
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Error creating window: %s\n", SDL_GetError());
-		return 1;
-	}
-
-	renderer = SDL_CreateRenderer(window, NULL);
-	if (renderer == NULL) {
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Error creating renderer: %s\n", SDL_GetError());
-		return 1;
+	if (!init_visualizer(&main_visualizer)) {
+		clear_program(&main_visualizer);
 	}
 
 	while (running) {
@@ -37,13 +31,40 @@ int main(int argc, char* argv[]) {
 				running = false;
 			}
 		}
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+		SDL_RenderClear(main_visualizer.renderer);
+		SDL_RenderPresent(main_visualizer.renderer);
 	}
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	clear_program(&main_visualizer);
 
 	return 0;
+}
+
+bool init_visualizer(struct Visualizer* visualizer_p) {
+	if (!SDL_Init(SDL_FLAGS)) {
+		fprintf(stderr, "Error initializing SDL library: %s\n", SDL_GetError());
+		return false;
+	}
+	printf("SDL library initalized !\n");
+
+	visualizer_p->window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+	if (visualizer_p->window == NULL) {
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Error creating window: %s\n", SDL_GetError());
+		return false;
+	}
+
+	visualizer_p->renderer = SDL_CreateRenderer(visualizer_p->window, NULL);
+	if (visualizer_p->renderer == NULL) {
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Error creating renderer: %s\n", SDL_GetError());
+		return false;
+	}
+	SDL_SetRenderDrawColor(visualizer_p->renderer, 100, 50, 75, SDL_ALPHA_OPAQUE);
+
+	return true;
+}
+
+void clear_program(struct Visualizer* visualizer_p) {
+	SDL_DestroyRenderer(visualizer_p->renderer);
+	SDL_DestroyWindow(visualizer_p->window);
+	SDL_Quit();
 }
